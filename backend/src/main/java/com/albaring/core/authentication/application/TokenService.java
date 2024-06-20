bbaef07ce7a1ed605b2185d0a0f9e73b221924f1
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-06-20 22:13:06 +0900

    test: 회원 인증 관련 레이어별 테스트 코드에 만족하는 기능 구현

---
+++ backend/src/main/java/com/albaring/core/authentication/application/TokenService.java
@@ -0,0 +1,34 @@
+package com.albaring.core.authentication.application;
+
+import com.albaring.core.authentication.application.dto.KakaoProfileResponse;
+import com.albaring.core.authentication.application.jwt.JwtTokenProvider;
+import com.albaring.core.authentication.application.kakao.KakaoClient;
+import com.albaring.core.authentication.domain.MemberTokens;
+import com.albaring.core.authentication.domain.RefreshToken;
+import com.albaring.core.authentication.domain.RefreshTokenRepository;
+import com.albaring.core.authentication.presentation.dto.KakaoCodeRequest;
+import com.albaring.core.member.application.MemberService;
+import com.albaring.core.member.domain.Member;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TokenService {
+
+  private final RefreshTokenRepository refreshTokenRepository;
+  private final MemberService memberService;
+  private final JwtTokenProvider jwtTokenProvider;
+  private final KakaoClient kakaoClient;
+
+  public MemberTokens generateToken(KakaoCodeRequest request) {
+    KakaoProfileResponse kakaoProfileResponse = kakaoClient.requestKakaoProfile(request.getCode());
+    Member member = memberService.findOrCreateMemberByKakaoId(kakaoProfileResponse.getId());
+
+    MemberTokens memberTokens = jwtTokenProvider.generateLoginToken(member.getId().toString());
+    RefreshToken savedRefreshToken = new RefreshToken(memberTokens.getRefreshToken(),
+        member.getId());
+    refreshTokenRepository.save(savedRefreshToken);
+    return memberTokens;
+  }
+}
