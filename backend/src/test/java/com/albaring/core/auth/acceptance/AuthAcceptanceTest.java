159e56230f8b732ad4390d8379b83c8ae42af2b2
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-06-26 12:19:03 +0900

    test: 로그아웃 관련 테스트 코드 추가

--- backend/src/test/java/com/albaring/core/auth/acceptance/AuthAcceptanceTest.java
+++ backend/src/test/java/com/albaring/core/auth/acceptance/AuthAcceptanceTest.java
@@ -1,6 +1,8 @@
 package com.albaring.core.auth.acceptance;
 
 import static com.albaring.core.auth.fixture.KakaoMemberFixture.라이언;
+import static com.albaring.core.auth.step.AuthSteps.로그아웃_요청;
+import static com.albaring.core.auth.step.AuthSteps.실패하는_로그아웃_요청;
 import static com.albaring.core.auth.step.AuthSteps.실패하는_로그인_요청;
 import static com.albaring.core.auth.step.AuthSteps.카카오_로그인_요청;
 import static com.albaring.core.auth.step.AuthSteps.토큰_확인;
@@ -21,7 +23,8 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         class 성공 {
 
             /**
-             * When 클라이언트는 카카오로부터 전달받은 정보로 로그인을 요청한다. Then 회원 가입 처리 후 토큰이 발급된다.
+             * When 클라이언트는 카카오로부터 전달받은 정보로 로그인을 요청한다.
+             * Then 회원 가입 처리 후 토큰이 발급된다.
              */
             @Test
             void 가입되지_않은_회원에게_토큰_발급() {
@@ -40,8 +43,8 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         class 실패 {
 
             /**
-             * When  클라이언트는 카카오로부터 발급받은 인가 코드를 포함하지 않고 로그인을 요청한다. Then  회원 가입이 진행되지 않고, 토큰이 발급되지
-             * 않는다.
+             * When  클라이언트는 카카오로부터 발급받은 인가 코드를 포함하지 않고 로그인을 요청한다.
+             * Then  회원 가입이 진행되지 않고, 토큰이 발급되지 않는다.
              */
             @Test
             void 인가_코드를_전달하지_않은_경우() {
@@ -57,19 +60,35 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         @Nested
         class 성공 {
 
+            /**
+             * Given 클라이언트는 카카오 로그인을 한다.
+             * When  리프레시 토큰을 포함해서 로그아웃을 요청한다.
+             * Then  정상적으로 로그아웃된다.
+             */
             @Test
             void 로그아웃_처리() {
                 // given
+                var 로그인_요청_정보 = new OauthProviderCodeRequest(라이언.인가_코드);
+                var 카카오_로그인_요청_응답 = 카카오_로그인_요청(로그인_요청_정보);
+                var 발급된_리프레시_토큰 = 카카오_로그인_요청_응답.cookie("refresh-token");
 
-                // when
-
-                // then
+                // when, then TODO: 추가 검증 방법 고민
+                로그아웃_요청(발급된_리프레시_토큰);
             }
         }
 
         @Nested
         class 실패 {
 
+            /**
+             * When  리프레시 토큰없이 로그아웃을 요청한다.
+             * Then  로그아웃에 실패한다.
+             */
+            @Test
+            void 로그아웃_처리() {
+                // when, then TODO: 검증 방법 고민
+                실패하는_로그아웃_요청();
+            }
         }
     }
 
