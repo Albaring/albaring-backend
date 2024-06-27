6bf0c99cdaef7f2fcf5686f914f39719bd0f4ba5
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-06-27 14:22:20 +0900

    feat: Naver API를 대체하는 Fake Naver Controller 구성
    - fix: 테스트 환경에서 Fake OAuth API를 구분하기 위한 수정

--- backend/src/test/java/com/albaring/core/auth/util/FakeKakaoController.java
+++ backend/src/test/java/com/albaring/core/auth/util/FakeKakaoController.java
@@ -2,8 +2,8 @@ package com.albaring.core.auth.util;
 
 
 import com.albaring.core.auth.fixture.KakaoMemberFixture;
-import com.albaring.core.authentication.application.dto.KakaoAccessTokenResponse;
-import com.albaring.core.authentication.application.dto.KakaoProfileResponse;
+import com.albaring.core.authentication.application.kakao.dto.KakaoAccessTokenResponse;
+import com.albaring.core.authentication.application.kakao.dto.KakaoProfileResponse;
 import com.albaring.core.authentication.application.jwt.BearerAuthorizationExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -22,7 +22,7 @@ public class FakeKakaoController {
     @Autowired
     BearerAuthorizationExtractor bearerAuthorizationExtractor;
 
-    @PostMapping(value = "/oauth/token", consumes = "application/x-www-form-urlencoded")
+    @PostMapping(value = "/kakao/oauth/token", consumes = "application/x-www-form-urlencoded")
     public ResponseEntity<KakaoAccessTokenResponse> createToken(
         @RequestParam("code") String code,
         @RequestParam("client_id") String clientId,
@@ -35,7 +35,7 @@ public class FakeKakaoController {
         return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping(value = "/v2/user/me")
+    @GetMapping(value = "/kakao/v2/user/me")
     public ResponseEntity<KakaoProfileResponse> findProfile(
         @RequestHeader(value = HttpHeaders.AUTHORIZATION) String accessToken,
         @RequestHeader(value = HttpHeaders.CONTENT_TYPE,
